97ab759e46dba7401067fe0f7efe72903a6d737b
parkej05 <64566903+parkej05@users.noreply.github.com>
2020-07-01 22:24:48 -0400

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ console.log("Hello! Welcome to my UX Design Portfolio")
 function menuToggle () {
     var x = document.getElementById ('myNavtoggle');
     if (x.className === 'navtoggle') {
-        x.className += 'responsive';
+        x.className += ' responsive';
     } else {
         x.className = 'navtoggle';
     }
